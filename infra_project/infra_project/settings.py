ba3b498b18a3a7b35eef36221b976d101adee94a
AntonGromtsev <fenix217grom@gmail.com>
2021-09-07 13:14:57 +0300

    Редакция settings

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "84.252.136.28",
+    "ttt-fenixgrom.ga",
+    "www.ttt-fenixgrom.ga",
+    "127.0.0.1",
+    "[::1]",
+    "testserver",
+]
 
 
 # Application definition
